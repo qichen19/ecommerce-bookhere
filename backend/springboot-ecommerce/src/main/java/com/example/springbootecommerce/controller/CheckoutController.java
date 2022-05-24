1c943fc64325ce78f40de632a249d6240775bcda
Qi Chen <qichen@Qis-MacBook-Pro.fios-router.home>
2022-05-24 10:58:45 -0400

    first init

---
+++ backend/springboot-ecommerce/src/main/java/com/example/springbootecommerce/controller/CheckoutController.java
@@ -0,0 +1,38 @@
+package com.example.springbootecommerce.controller;
+
+import com.example.springbootecommerce.dto.PaymentInfo;
+import com.example.springbootecommerce.dto.Purchase;
+import com.example.springbootecommerce.dto.PurchaseResponse;
+import com.example.springbootecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    @Autowired
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+        PurchaseResponse purchaseResponse = this.checkoutService.placeOrder(purchase);
+        return purchaseResponse;
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+        String paymentStr = paymentIntent.toJson();
+
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
+    }
+}
